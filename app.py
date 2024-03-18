70c887e2db1ada929bdfb0486c1dd1cb8afae842
Hongye Zhang <77666748+hongye-zhang@users.noreply.github.com>
2024-03-18 20:20:37 +0100

    update

--- app.py
+++ app.py
@@ -1,5 +1,40 @@
-from flask import Flask
+
+from flask import Flask, request
+import shutil
+import os
+import supabase
+
 app = Flask(__name__)
+
+
+@app.route('/uploadpdf', methods=['POST'])
+def upload_pdf():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return {"detail": "No file part in request."}, 400
+
+    file = request.files['file']
+
+    # if no file is selected
+    if file.filename == '':
+        return {"detail": "No selected file."}, 400
+
+    if not file.filename.endswith(".pdf"):
+        return {"detail": "Invalid file type. PDF file expected."}, 400
+
+    if file:
+        filename = file.filename
+        file_path = os.path.join(os.getcwd(), filename)
+
+        # save file to disk
+        file.save(file_path)
+        try:
+            supabase.storage.from_("testbucket").upload(file=file, path="https://supabase.com/dashboard/project/tdklrrxdggwsbfdvtlws/storage/buckets/PDF%20storage",
+                                                    file_options={"content-type": "pdf"})
+        except:
+            b = 0
+        return {"filename": filename, "message": "File saved locally."}, 200
+
 
 @app.route('/')
 def hello_world():
