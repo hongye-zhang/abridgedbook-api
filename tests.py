330890859dc86868a7275c4e57d628b5ba1b4532
Hongye Zhang <77666748+hongye-zhang@users.noreply.github.com>
2024-04-02 21:26:06 +0200

    full deploy

--- tests.py
+++ tests.py
@@ -2,19 +2,17 @@ import requests
 
 
 def call_upload_pdf(path):
-    url = "https://abridgedbook-api.onrender.com/uploadpdf"  # replace with your Flask app's URL
+    url = "http://127.0.0.1:5000/uploadpdf"  # replace with your Flask app's URL
 
-    # prepare headers for http request
-    headers = {
-        "Content-Type": "application/pdf",
-    }
+
 
     with open(path, "rb") as f:
-        response = requests.post(url, headers=headers, files={"file": f})
+        files = {"file": f}
+        response = requests.post(url, files=files)
     print(response.status_code)
     print(response.text)
     return response
 
 
-resp = call_upload_pdf('C:/Users/hongy/Documents/Work/Knowledgebase/refine/AW/AW.pdf')
+resp = call_upload_pdf("AW.pdf")
 print(resp)
